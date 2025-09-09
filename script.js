c993c07a2dbfd6455862df04feb95194394005f9
mingming0324 <alsehfdl14@gmail.com>
2025-09-09 11:18:29 +0900

    애니메이션 추가

--- script.js
+++ script.js
@@ -1,4 +1,73 @@
 gsap.registerPlugin(ScrollTrigger);
+
+//sec-1 마우스 오버 따라가기
+function MousemoveEffect1__start() {
+  const $window = $(window);
+
+  let windowWidth = $window.width();
+  let windowHeight = $window.height();
+
+  $window.resize(_.throttle(function () {
+    windowWidth = $window.width();
+    windowHeight = $window.height();
+  }, 100));
+
+  $window.resize(_.throttle(function () {
+    MousemoveEffect1__update();
+  }, 100));
+
+  let MousemoveEffect1__$el = null;
+  let MousemoveEffect1__lastPosX = 0;
+  let MousemoveEffect1__lastPosY = 0;
+
+  function MousemoveEffect1__update() {
+    MousemoveEffect1__$el.each(function (index, node) {
+      const $node = $(node);
+      const horRes = $node.data('data-mousemove-effect1-hor-res');
+      const verRes = $node.data('data-mousemove-effect1-ver-res');
+
+      const x = (MousemoveEffect1__lastPosX - (windowWidth / 2)) * horRes;
+      const y = (MousemoveEffect1__lastPosY - (windowHeight / 2)) * verRes;
+      $(node).css('transform', 'translateX(' + x + 'px) translateY(' + y + 'px)');
+
+      console.log("MousemoveEffect1__lastPosX : " + MousemoveEffect1__lastPosX);
+      console.log("MousemoveEffect1__lastPosY : " + MousemoveEffect1__lastPosY);
+    });
+  }
+  console.log(
+    $('.glass').map((i,el)=>[
+      $(el).data('data-mousemove-effect1-hor-res'),
+      $(el).data('data-mousemove-effect1-ver-res')
+    ]).get()
+  );
+
+  function MousemoveEffect1__init() {
+    MousemoveEffect1__$el = $('.glass');
+
+    MousemoveEffect1__$el.each(function (index, node) {
+      const $node = $(node);
+      $node.data('data-mousemove-effect1-hor-res', $node.attr('data-mousemove-effect1-hor-res') * 1);
+      $node.data('data-mousemove-effect1-ver-res', $node.attr('data-mousemove-effect1-ver-res') * 1);
+    });
+
+    const MousemoveEffect1__updateThrottled = _.throttle(function () {
+      MousemoveEffect1__update();
+    }, 10);
+
+    $window.mousemove(function (e) {
+      MousemoveEffect1__lastPosX = e.clientX;
+      MousemoveEffect1__lastPosY = e.clientY;
+
+      MousemoveEffect1__updateThrottled();
+    });
+  }
+  
+  MousemoveEffect1__init();
+}
+
+MousemoveEffect1__start();
+
+// -sec-1 유리-심한 블러버전
 // const feImage = document.querySelector("feImage");
 // fetch("https://essykings.github.io/JavaScript/map.png")
 //   .then((response) => {
@@ -10,6 +79,7 @@ gsap.registerPlugin(ScrollTrigger);
 //     feImage.setAttribute("href", objURL);
 //   });
 
+// sec-2 스크롤 시 글자 채워지기
 let tl_2 = gsap.timeline({
   scrollTrigger: {
     trigger: ".sec-2",       
@@ -25,7 +95,6 @@ let tl_2 = gsap.timeline({
 
 tl_2.to(".sec2-text-box p", {
   backgroundSize: '100% 100%',
-
 });
 
 
@@ -53,7 +122,7 @@ tl_2.to(".sec2-text-box p", {
         },
       },
   });
-
+//sec-3 글자 애니메이션
   let tl_1 = gsap.timeline({
     scrollTrigger: {
       trigger: ".sec-3",       
@@ -73,6 +142,8 @@ tl_2.to(".sec2-text-box p", {
     opacity: 1,
   });
 
+
+
 // sec-6 스와이퍼 
 var swiper = new Swiper(".mySwiper-2", {
   watchSlidesProgress: true,
@@ -80,3 +151,17 @@ var swiper = new Swiper(".mySwiper-2", {
   loop: true,
   spaceBetween: 78,
 });
+
+
+$(document)
+  .on('mouseenter', '.sec-6_img-box', function(){ $(this).addClass('is-hover'); })
+  .on('mouseleave', '.sec-6_img-box', function(){ $(this).removeClass('is-hover'); });
+// $(".sec-6_img-box").mouseenter(function() {
+//   let $this = $(this);
+//   $this.addClass("is-hover") 
+// });
+
+// $(".sec-6_img-box").mouseleave(function() {
+//   let $this = $(this);
+//   $this.removeClass("is-hover") 
+// });
